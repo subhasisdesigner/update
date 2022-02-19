cc8466ea1fb7b81e04fe84c24ac168ea41d64475
subhasis santra <santrasubhasis15@gmail.com>
2022-02-19 16:06:56 +0530

    under construction update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ ReactDOM.render(
   <div class="loading-wrapper">
     <div class="loader-container">
         <div class="state-pending">
-        <div class="copy_right"><i class="ri-quill-pen-fill"></i><span>Design by <strong>Subhasis Santra</strong></span></div>
             <img src={underBuildPic}/>
         </div>
     </div>
