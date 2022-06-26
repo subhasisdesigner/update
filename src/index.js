587536764b48b73b99b4c0a45f46afbdc21bc7a7
subhasis santra <santrasubhasis15@gmail.com>
2022-06-26 12:49:05 +0530

    file structure change

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
-import underBuildPic from './asset/construction.png'
+import { BrowserRouter } from 'react-router-dom';
+import UnderWork from './under_working';
 
 
 ReactDOM.render(
   <BrowserRouter>
-  {/* <div id="under_duild">
-  <div className="loading-wrapper">
-    <div className="loader-container">
-        <div className="state-pending">
-          <span className="Under">Under Construction</span>  
-          <img src={underBuildPic}/>
-        </div>
-    </div>
-</div>
-  </div> */}
+    <UnderWork />
     <App />
   </BrowserRouter>,
   document.getElementById('root')
