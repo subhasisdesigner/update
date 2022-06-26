9f634383f76cd7477ac493d8bc23292836ca4baf
subhasis santra <santrasubhasis15@gmail.com>
2022-06-26 15:04:17 +0530

    gitignore file update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import UnderWork from './under_working';
+import UnderWork from './underWorking';
 
 
 ReactDOM.render(
