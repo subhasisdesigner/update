9a4ed17af74cef28af021ddfe72806a52eb54521
subhasis santra <santrasubhasis15@gmail.com>
2022-07-18 00:44:03 +0530

    out under

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import UnderWork from './underWorking';
 
 
 ReactDOM.render(
   <BrowserRouter>
-    <UnderWork />
     <App />
   </BrowserRouter>,
   document.getElementById('root')
